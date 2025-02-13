de0b6cc75bf54a6351af9187a1f0632787bbc622
Furini00 <mattia01.furini@edu.unife.it>
2025-02-13 18:25:48 +0100

    modificato hello.ts

--- hello.ts
+++ hello.ts
@@ -1,2 +1,6 @@
-console.log("Hello, GitHub Actions!");
+function greet(name: string): void {
+    console.log(`Hello, ${name}! Welcome to TypeScript.`);
+}
 
+// Esegui la funzione con un nome di esempio
+greet("Furini");
